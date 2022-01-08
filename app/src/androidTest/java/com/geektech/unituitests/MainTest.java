1936662883ddfff57e3be214fd80af5d7a4e2fe6
Nursamad <bagishbekow@gmail.com>
2022-01-08 18:23:47 +0600

    fix_bag

--- app/src/androidTest/java/com/geektech/unituitests/MainTest.java
+++ app/src/androidTest/java/com/geektech/unituitests/MainTest.java
@@ -2,6 +2,7 @@ package com.geektech.unituitests;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
@@ -20,7 +21,6 @@ public class MainTest {
 
     @Rule
     public ActivityScenarioRule rule = new ActivityScenarioRule<>(MainActivity.class);
-
     @Test
     public void checkAddOperations() {
         onView(withId(R.id.firstNum_edt)).perform(typeText("5"));
@@ -29,7 +29,6 @@ public class MainTest {
         onView(withId(R.id.tv_result)).check(matches(withText("11")));
     }
 
-
     @Test
     public void checkAddOperationsWithEmptyField() {
         onView(withId(R.id.firstNum_edt)).perform(typeText(""));
@@ -37,76 +36,10 @@ public class MainTest {
         onView(withId(R.id.btn_add)).perform(click());
         onView(withId(R.id.tv_result)).check(matches(withText("6")));
     }
-
-    @Test
-    public void checkAddNegativeOperation(){
-        onView(withId(R.id.firstNum_edt)).perform(typeText("-3"));
-        onView(withId(R.id.secondNum_edt)).perform(typeText("-3"));
-        onView(withId(R.id.btn_add)).perform(click());
-        onView(withId(R.id.tv_result)).check(matches(withText("-6")));
-    }
-
     @Test
     public void checkAddWithOneEmptyOperation(){
         onView(withId(R.id.firstNum_edt)).perform(typeText(""));
         onView(withId(R.id.secondNum_edt)).perform(typeText("3"));
-        onView(withId(R.id.btn_add)).perform(click());
-        onView(withId(R.id.tv_result)).check(matches(withText("3")));
-    }
-    @Test
-    public void checkAddWithAllEmptyOperation(){
-        onView(withId(R.id.firstNum_edt)).perform(typeText(""));
-        onView(withId(R.id.secondNum_edt)).perform(typeText(""));
-        onView(withId(R.id.btn_add)).perform(click());
-        onView(withId(R.id.tv_result)).check(matches(withText("")));
-    }
-    @Test
-    public void checkDivByZero(){
-        onView(withId(R.id.firstNum_edt)).perform(typeText("4"));
-        onView(withId(R.id.secondNum_edt)).perform(typeText("0"));
-        onView(withId(R.id.btn_add)).perform(click());
-        onView(withId(R.id.tv_result)).check(matches(withText("0")));
-    }
-    @Test
-    public void checkMultyByZero(){
-        onView(withId(R.id.firstNum_edt)).perform(typeText("6"));
-        onView(withId(R.id.secondNum_edt)).perform(typeText("0"));
-        onView(withId(R.id.btn_add)).perform(click());
-        onView(withId(R.id.tv_result)).check(matches(withText("0")));
-    }
-    @Test
-    public void checkNegativeMultyNeg(){
-        onView(withId(R.id.firstNum_edt)).perform(typeText("-2"));
-        onView(withId(R.id.secondNum_edt)).perform(typeText("-2"));
-        onView(withId(R.id.btn_add)).perform(click());
-        onView(withId(R.id.tv_result)).check(matches(withText("4")));
-    }
-
-    @Test
-    public void checkPositiveMultyNegative(){
-        onView(withId(R.id.firstNum_edt)).perform(typeText("3"));
-        onView(withId(R.id.secondNum_edt)).perform(typeText("-2"));
-        onView(withId(R.id.btn_add)).perform(click());
-        onView(withId(R.id.tv_result)).check(matches(withText("-6")));
-    }
-    @Test
-    public void checkSimpleMultyCase(){
-        onView(withId(R.id.firstNum_edt)).perform(typeText("3"));
-        onView(withId(R.id.secondNum_edt)).perform(typeText("2"));
-        onView(withId(R.id.btn_add)).perform(click());
-        onView(withId(R.id.tv_result)).check(matches(withText("6")));
-    }
-    @Test
-    public void checkDivSameDigit(){
-        onView(withId(R.id.firstNum_edt)).perform(typeText("3"));
-        onView(withId(R.id.secondNum_edt)).perform(typeText("3"));
-        onView(withId(R.id.btn_add)).perform(click());
-        onView(withId(R.id.tv_result)).check(matches(withText("1")));
-    }
-    @Test
-    public void checkNumMultyByOne(){
-        onView(withId(R.id.firstNum_edt)).perform(typeText("3"));
-        onView(withId(R.id.secondNum_edt)).perform(typeText("1"));
         onView(withId(R.id.btn_add)).perform(click());
         onView(withId(R.id.tv_result)).check(matches(withText("3")));
     }
@@ -118,12 +51,71 @@ public class MainTest {
         onView(withId(R.id.tv_result)).check(matches(withText("0")));
     }
     @Test
-    public void checkMinusMultyMinus(){
-        onView(withId(R.id.firstNum_edt)).perform(typeText("-2"));
-        onView(withId(R.id.secondNum_edt)).perform(typeText("-2"));
+    public void checkAddWithAllEmptyOperation(){
+        onView(withId(R.id.firstNum_edt)).perform(typeText("0"));
+        onView(withId(R.id.secondNum_edt)).perform(typeText("0"));
         onView(withId(R.id.btn_add)).perform(click());
         onView(withId(R.id.tv_result)).check(matches(withText("0")));
     }
 
 
+
+    @Test
+    public void checkDivByZero(){
+        onView(withId(R.id.firstNum_edt)).perform(typeText("4"));
+        onView(withId(R.id.secondNum_edt)).perform(typeText("0"), closeSoftKeyboard());
+        onView(withId(R.id.btn_div)).perform(click());
+        onView(withId(R.id.tv_result)).check(matches(withText("0")));
+    }
+    @Test
+    public void checkMultyByZero(){
+        onView(withId(R.id.firstNum_edt)).perform(typeText("6"));
+        onView(withId(R.id.secondNum_edt)).perform(typeText("0"), closeSoftKeyboard());
+        onView(withId(R.id.btn_mult)).perform(click());
+        onView(withId(R.id.tv_result)).check(matches(withText("0")));
+    }
+    @Test
+    public void checkNegativeMultyNeg(){
+        onView(withId(R.id.firstNum_edt)).perform(typeText("-2"));
+        onView(withId(R.id.secondNum_edt)).perform(typeText("-2"), closeSoftKeyboard());
+        onView(withId(R.id.btn_mult)).perform(click());
+        onView(withId(R.id.tv_result)).check(matches(withText("4")));
+    }
+
+    @Test
+    public void checkPositiveMultyNegative(){
+        onView(withId(R.id.firstNum_edt)).perform(typeText("3"));
+        onView(withId(R.id.secondNum_edt)).perform(typeText("-2"), closeSoftKeyboard());
+        onView(withId(R.id.btn_mult)).perform(click());
+        onView(withId(R.id.tv_result)).check(matches(withText("-6")));
+    }
+    @Test
+    public void checkSimpleMultyCase(){
+        onView(withId(R.id.firstNum_edt)).perform(typeText("3"));
+        onView(withId(R.id.secondNum_edt)).perform(typeText("2"), closeSoftKeyboard());
+        onView(withId(R.id.btn_mult)).perform(click());
+        onView(withId(R.id.tv_result)).check(matches(withText("6")));
+    }
+    @Test
+    public void checkDivSameDigit(){
+        onView(withId(R.id.firstNum_edt)).perform(typeText("3"));
+        onView(withId(R.id.secondNum_edt)).perform(typeText("3"), closeSoftKeyboard());
+        onView(withId(R.id.btn_div)).perform(click());
+        onView(withId(R.id.tv_result)).check(matches(withText("1")));
+    }
+    @Test
+    public void checkNumMultyByOne(){
+        onView(withId(R.id.firstNum_edt)).perform(typeText("3"));
+        onView(withId(R.id.secondNum_edt)).perform(typeText("1"), closeSoftKeyboard());
+        onView(withId(R.id.btn_mult)).perform(click());
+        onView(withId(R.id.tv_result)).check(matches(withText("3")));
+    }
+
+    @Test
+    public void checkMinusMultyMinus(){
+        onView(withId(R.id.firstNum_edt)).perform(typeText("-2"));
+        onView(withId(R.id.secondNum_edt)).perform(typeText("-2"), closeSoftKeyboard());
+        onView(withId(R.id.btn_mult)).perform(click());
+        onView(withId(R.id.tv_result)).check(matches(withText("4")));
+    }
 }
